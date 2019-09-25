d24c5428847e54e818a6a2b7d28103bcc77d73a6
kim-ly <SoSe2018>
2019-09-25 13:17:36 +0200

    Enables location object pop up

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import FetchLocation from './components/FetchLocation';
+import Geolocation from '@react-native-community/geolocation';
 
 export default function App() {
+  getUserLocationHandler = () => {
+      Geolocation.getCurrentPosition(
+        position => { console.log(position);
+        }, err => console.log(err),
+        { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+        );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <FetchLocation onGetLocation={this.getUserLocationHandler}/>
     </View>
   );
 }
